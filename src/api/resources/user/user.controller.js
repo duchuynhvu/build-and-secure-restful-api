ca2242aebe4cc4e9db43dfc230e0164da1711b48
huynhvu <huynh.vu01@umt.edu.vn>
2021-12-24 11:37:00 +0700

    user registration

--- src/api/resources/user/user.controller.js
+++ src/api/resources/user/user.controller.js
@@ -1,11 +1,24 @@
 import userService from "./user.service";
+import User from "./user.model";
 
 export default {
     async signup(req, res) {
-        const { value, error } = userService.validateSignup(req.body);
-        if (error) {
-            return res.status(400).json(error);
+        try {
+            const { value, error } = userService.validateSignup(req.body);
+            if (error) {
+                return res.status(400).json(error);
+            }
+            const encryptedPass = userService.encryptPassword(value.password);
+            const user = await User.create({
+                firstName: value.firstName,
+                lastName: value.lastName,
+                email: value.email,
+                password: encryptedPass,
+            });
+            return res.json({ success: true });
+        } catch (err) {
+            console.error(err);
+            return res.status(500).send(err);
         }
-        return res.json(value);
     }
 }
