ec4bafc64bdb75798b459d1a25c60248328bfc38
huynhvu <huynh.vu01@umt.edu.vn>
2021-12-27 15:13:43 +0700

    updated for authenticated user can view all the songs

--- src/api/resources/song/song.router.js
+++ src/api/resources/song/song.router.js
@@ -12,9 +12,9 @@ const artistPolicy = [passport.authenticate('jwt', { session: false }), isArtist
 songRouter
     .route('/')
     .post(artistPolicy, songController.create)
-    .get(artistPolicy, songController.findAll);
+    .get(passport.authenticate('jwt', { session: false }), songController.findAll);
 
 songRouter.route('/:id')
-    .get(artistPolicy, songController.findOne)
+    .get(passport.authenticate('jwt', { session: false }), songController.findOne)
     .delete(artistPolicy, songController.delete)
     .put(artistPolicy, songController.update);
