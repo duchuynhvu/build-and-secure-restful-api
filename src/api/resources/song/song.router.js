68db894dff82cc8d86d9e3b18f701ed2357822c9
huynhvu <huynh.vu01@umt.edu.vn>
2021-12-27 13:34:09 +0700

    role based authentication

--- src/api/resources/song/song.router.js
+++ src/api/resources/song/song.router.js
@@ -1,17 +1,20 @@
 import express from 'express';
 import songController from './song.controller';
 import passport from 'passport';
+import { isArtist } from '../../middlewares/is-artist';
 
 export const songRouter = express.Router();
 // 1. authenticated user can view all the songs
 // 2. an artist can create, update, and delete song
 
+const artistPolicy = [passport.authenticate('jwt', { session: false }), isArtist];
+
 songRouter
     .route('/')
-    .post(passport.authenticate('jwt', { session: false }), songController.create)
-    .get(passport.authenticate('jwt', { session: false }), songController.findAll);
+    .post(artistPolicy, songController.create)
+    .get(artistPolicy, songController.findAll);
 
 songRouter.route('/:id')
-    .get(passport.authenticate('jwt', { session: false }), songController.findOne)
-    .delete(passport.authenticate('jwt', { session: false }), songController.delete)
-    .put(passport.authenticate('jwt', { session: false }), songController.update);
+    .get(artistPolicy, songController.findOne)
+    .delete(artistPolicy, songController.delete)
+    .put(artistPolicy, songController.update);
