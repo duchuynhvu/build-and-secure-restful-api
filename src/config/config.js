7462dca4a42ea43bec19b48362b6f06ecb98594f
huynhvu <huynh.vu01@umt.edu.vn>
2021-12-29 11:06:11 +0700

    connect to remote db from local

--- src/config/config.js
+++ src/config/config.js
@@ -6,6 +6,7 @@ const config = {
     },
     development: {
         secret: 'I_AME_GERER',
+        // MONGO_URI: 'mongodb+srv://root:p3ssw0rd@cluster0.xhd6d.mongodb.net/umt-tuyensinh-db',
         MONGO_URI: 'mongodb://localhost/umt-tuyensinh-db',
         port: 3000
     }
