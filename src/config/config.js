61000c8f49ff7674da9ef1b53b7cbcacd1108ebc
huynhvu <huynh.vu01@umt.edu.vn>
2021-12-29 11:22:21 +0700

    change the name of project, db, doc

--- src/config/config.js
+++ src/config/config.js
@@ -6,8 +6,8 @@ const config = {
     },
     development: {
         secret: 'I_AME_GERER',
-        // MONGO_URI: 'mongodb+srv://root:p3ssw0rd@cluster0.xhd6d.mongodb.net/umt-tuyensinh-db',
-        MONGO_URI: 'mongodb://localhost/umt-tuyensinh-db',
+        // MONGO_URI: 'mongodb+srv://root:p3ssw0rd@cluster0.xhd6d.mongodb.net/music-db',
+        MONGO_URI: 'mongodb://localhost/music-db',
         port: 3000
     }
 };
